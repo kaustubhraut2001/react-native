8d53e14ef1b6c8d9e54a5e5d8de2abe0a7d9ad9b
Kaustubh <kaustubhr2001@gmail.com>
2024-02-13 00:22:00 +0530

    some updates

--- App.tsx
+++ App.tsx
@@ -1,30 +1,31 @@
-import { StatusBar, StyleSheet, Text, Touchable , TouchableOpacity, View } from 'react-native'
-import React, {useState} from 'react'
+import { StatusBar, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 
 const App = () => {
-  const [colors , setColors] = useState('#ffffff');
-  const genreatecolor = ()=>{
+  const [colors, setColors] = useState('#ffffff');
+  const genreatecolor = () => {
     const hexrange = '0123456789ABCDEF';
     let hexcolor = '#';
-    for(let i = 0; i<6; i++){
+    for (let i = 0; i < 6; i++) {
       hexcolor += hexrange[Math.floor(Math.random() * 16)];
     }
-   setColors(hexcolor);
+    setColors(hexcolor);
   }
   return (
     <>
-   <StatusBar backgroundColor={colors} />
-   <View style={[styles.conatiner , {backgroundColor : colors}]} >
-    <TouchableOpacity onPress={() => genreatecolor()}>
-      <View style={styles.pressme} >
-      <Text style={styles.stylebtn}>Press Me!</Text>
+      <StatusBar backgroundColor={colors} />
+      <View style={[styles.conatiner, { backgroundColor: colors }]} >
+        <TouchableOpacity onPress={() => genreatecolor()}>
+          <Text style={styles.shade}> Circle </Text>
+          <View style={styles.pressme} >
+            <Text style={styles.stylebtn}>Press Me!</Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    </TouchableOpacity>
-   </View>
-   </>
+    </>
 
   )
 }
@@ -36,16 +37,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  
+
   },
+
   pressme: {
+
     backgroundColor: 'gray',
+
     padding: 10,
-    borderRadius: 5
+    borderRadius: 50
   },
+  shade: {
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    padding: 20,
+    marginBottom: 60,
+    borderRadius: 150,
+    
+  },
+
   stylebtn: {
     color: 'white',
-    fontSize: 20
+    fontSize: 20,
+
   }
 
 
