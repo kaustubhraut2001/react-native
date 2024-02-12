a9794edfe206628fb088524ca333c35c91ff8609
Kaustubh <kaustubhr2001@gmail.com>
2024-02-13 00:08:38 +0530

    basic project

--- App.tsx
+++ App.tsx
@@ -1,17 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, Touchable , TouchableOpacity, View } from 'react-native'
 import React, {useState} from 'react'
 
 
 
 const App = () => {
-  const [colors , setColors] = useState();
+  const [colors , setColors] = useState('#ffffff');
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+   <StatusBar backgroundColor={colors} />
+   <View style={styles.conatiner}>
+    <TouchableOpacity onPress={() => setColors('black')}>
+      <View style={styles.pressme}>
+      <Text style={styles.stylebtn}>Press Me!</Text>
+      </View>
+    </TouchableOpacity>
+   </View>
+   </>
+
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  conatiner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  pressme: {
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5
+  },
+  stylebtn: {
+    color: 'white',
+    fontSize: 20
+  }
+
+
+
+})
